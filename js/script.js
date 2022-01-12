6c5a3634eab7fbca34d6ee75e5a0e5f61aa44b73
marcodeiulio <mdeiulio@icloud.com>
2022-01-12 17:11:17 +0100

    - Added name recovery - Added distance recovery - Added Age recovery - Added ticket calculation

--- js/script.js
+++ js/script.js
@@ -7,59 +7,55 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 */
 
-const alertNumber = `Il valore ammesso non è un numero!
-Per favore, inserire solo numeri.`;
 
-const alertName = 'Dati inseriti non validi.'
+const get = document.getElementById('get');
 
-const userName = prompt('Inserire nome e cognome.', 'Gennarino Gennaretti').trim();
+get.addEventListener('click',
+	function () {
 
-if (!userName.length) {
-	alert(alertName);
-	location.reload();
-} else {
-	document.getElementById('user-name').innerText = userName;
-	console.log('Nome utente: ' + userName);
-}
+		//# Nome
+		const userName = document.getElementById('name').value;
+		console.log(userName);
+		if (!userName.length) {
+			alert('Nome inserito non valido.');
+			location.reload();
+		} else {
+			document.getElementById('user-name').innerText = userName;
+			console.log('Nome utente: ' + userName);
+		}
 
-const userAge = parseInt(prompt(`Quanti anni hai?
-Solo numeri ammessi.`, '25').trim());
-console.log('Età utente: ', userAge);
+		//# Distanza
+		const userDistance = parseInt(document.getElementById('kms').value);
+		if (isNaN(userDistance) || (userDistance === 0)) {
+			alert('Distanza inserita non valida.');
+			location.reload();
+		} else if (userDistance > 2000) {
+			alert('Distanza troppo elevata.');
+			location.reload();
+		}
+		console.log('Distanza da percorrere: ', userDistance);
+		console.log(userDistance);
 
-if (isNaN(userAge)) {
-	alert(alertNumber);
-	location.reload();
-} else if (userAge > 120) {
-	alert('Età non valida.');
-	location.reload();
-}
+		//# Età
+		const userAge = document.getElementById('age').value;
+		console.log(userAge);
 
-const userDistance = parseInt(prompt(`Per quanti chilometri vuoi viaggiare?
-Inserire un valore espresso in KM.
-Solo numeri ammessi.`, '50').trim());
-console.log('Distanza da percorrere: ', userDistance);
 
-if (isNaN(userDistance)) {
-	alert(alertNumber);
-	location.reload();
-} else if (userDistance > 2000) {
-	alert('Distanza troppo elevata.');
-	location.reload();
-}
+		//# Calcolo biglietto
+		let ticketPrice = userDistance * 0.21;
+		console.log('Prezzo del biglietto: ', ticketPrice);
 
-let ticketPrice = userDistance * 0.21;
-console.log('Prezzo del biglietto: ', ticketPrice);
+		if (userAge < 18) {
+			ticketPrice = ticketPrice * 0.80;
+			document.getElementById('offer').innerText = 'Junior'
+		} else if (userAge > 65) {
+			ticketPrice = ticketPrice * 0.60;
+			document.getElementById('offer').innerText = 'Senior'
+		} else {
+			document.getElementById('offer').innerText = 'Standard'
+		}
 
-if (userAge < 18) {
-	ticketPrice = ticketPrice * 0.80;
-	document.getElementById('offer').innerText = 'Junior'
-} else if (userAge > 65) {
-	ticketPrice = ticketPrice * 0.60;
-	document.getElementById('offer').innerText = 'Senior'
-} else {
-	document.getElementById('offer').innerText = 'Standard'
-}
+		document.getElementById('ticket-price').innerText = ticketPrice.toFixed(2) + '€';
+		console.log('Prezzo del biglietto scontato: ', ticketPrice.toFixed(2), '€');
+	});
 
-const priceMessage = ticketPrice.toFixed(2) + '€';
-document.getElementById('ticket-price').innerText = priceMessage;
-console.log('Prezzo del biglietto scontato: ', ticketPrice.toFixed(2), '€');
